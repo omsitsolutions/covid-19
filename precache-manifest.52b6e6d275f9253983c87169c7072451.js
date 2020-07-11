b02a3110d4084ebda89e81c9fc54c1cbfc01031e
NITIN TAYAL <er.ntayal@gmail.com>
2020-07-11 19:55:05 +0530

    Updates

---
+++ precache-manifest.52b6e6d275f9253983c87169c7072451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc31fe66d6398deb6b08ffcc9c197a2",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "e34e703ce5d6792b6f50",
+    "url": "/covid-19/static/js/2.da06e9fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19/static/js/2.da06e9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd4949308ad06468717",
+    "url": "/covid-19/static/js/main.07e71fff.chunk.js"
+  },
+  {
+    "revision": "2ad4dd477122ea9994fd",
+    "url": "/covid-19/static/js/runtime-main.ca2dbecb.js"
+  }
+]);
